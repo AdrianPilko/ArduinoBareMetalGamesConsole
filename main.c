a0aa28d0c6064767c298747dba52f697943a60b2
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-10-28 20:25:12 +0100

    added more of TV signal
    PAL tv signal now has hsync and vsync, but still haven't handled the 2 fields totaling 625 hsync lines at 50 vsync per seconds

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include<avr/io.h>
 #include <util/delay.h>
 
-#define COMPOSITE_PIN PD5
-#define HSYNC_BACKPORCH 5
+#define COMPOSITE_PIN PB5
+#define LUMINANCE_PIN PB4
+#define MAX_LINE_BEFORE_BLANK 312
+
+#define HSYNC_BACKPORCH 4
 
 int main()
 {
-	DDRB |= 1 << 5;         //configure led as input
+	uint16_t lineCounter = 0;
+	uint8_t vSync = 0;
+	uint8_t i = 0;
+
+	DDRB |= 1 << COMPOSITE_PIN;         //configure led as input
+	DDRB |= 1 << LUMINANCE_PIN;
+
 	TCCR1B = (1<<CS10);// | (1<<CS12); //set the pre-scalar as 1024
-	OCR1A = 512; 	   //512usec delay
+	OCR1A = 805; 	   //usec delay
 	TCNT1 = 0;
 	while(1)
 	{
@@ -17,11 +26,36 @@ int main()
 		{
 				// wait till the timer overflow flag is SET
 		}
-		PORTB = 0;
-		DDRB |= 1 << 5;         //configure led as input
-		_delay_us(HSYNC_BACKPORCH);
-		DDRB &= ~(1 << 5);         //configure led as input
-		PORTB = 1;
+
+
+		if (vSync) // invert the line sync pulses when in vsync part of screen
+		{
+			DDRB &= ~(1 << COMPOSITE_PIN);
+			for (i = 0; i < 18; i++)
+			{
+				__asm__ __volatile__ ("nop");
+			}
+			PORTB = 0;
+			DDRB |= (1 << COMPOSITE_PIN);
+		}
+		else
+		{
+			PORTB = 0; // set all PORTB to low, as DDRB in next statement is input this causes output to go low
+			DDRB |= (1 << COMPOSITE_PIN); // set COMPOSITE_PIN as output
+			for (i = 0; i < 18; i++)
+			{
+				__asm__ __volatile__ ("nop");
+			}
+			DDRB &= ~(1 << COMPOSITE_PIN); // set COMPOSITE_PIN as input
+			PORTB = 1; // set all PORTB to high, as DDRB in input this causes output to go high
+		}
+		lineCounter++;
+		switch (lineCounter)
+		{
+			case 1: vSync = 0; break;
+			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; break;
+			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; break;
+		}
 
 		TCNT1 = 0;
 		TIFR1 |= (1<<OCF1A) ; //clear timer1 overflow flag
