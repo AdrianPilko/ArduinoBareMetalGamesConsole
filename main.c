f6202b131fe69bb8c03cd49b18ee72ae62b42e49
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-10-29 09:39:44 +0100

    more main.c updates

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+// PAL video output using atmega328p.
+// can be ran on arduino board, pin and 12 and 13 go to centre of a 10k pot, pin 12 through a 330 Ohm resistor.
+// one side of the pot goes to VCC the other to ground and the centre of the pot also goes to the composite connector
+// centre pin. if using atmega328p on bare board (not arduino) then its PB5 and PB4.
+
 #include<avr/io.h>
 #include <util/delay.h>
 
@@ -5,7 +10,7 @@
 #define LUMINANCE_PIN PB4
 #define MAX_LINE_BEFORE_BLANK 312
 
-#define HSYNC_BACKPORCH 4
+#define HSYNC_BACKPORCH 18
 
 int main()
 {
@@ -13,11 +18,13 @@ int main()
 	uint8_t vSync = 0;
 	uint8_t i = 0;
 	uint8_t	pixels = 1;
+	uint8_t	xPos
+	uint8_t	barStartPosition = 90;
 
-	DDRB |= 1 << COMPOSITE_PIN;         //configure led as input
+	DDRB |= 1 << COMPOSITE_PIN;
 	DDRB |= 1 << LUMINANCE_PIN;
 
-	TCCR1B = (1<<CS10);// | (1<<CS12); //set the pre-scalar as 1024
+	TCCR1B = (1<<CS10); // switch off clock prescaller
 	OCR1A = 1025; 	   //usec delay
 	TCNT1 = 0;
 	while(1)
@@ -36,7 +43,7 @@ int main()
 		if (vSync > 0) // invert the line sync pulses when in vsync part of screen
 		{
 			DDRB &= ~(1 << COMPOSITE_PIN);
-			for (i = 0; i < 18; i++)
+			for (i = 0; i < HSYNC_BACKPORCH; i++)
 			{
 				__asm__ __volatile__ ("nop");
 			}
@@ -52,7 +59,7 @@ int main()
 			DDRB &= ~(1 << LUMINANCE_PIN);
 			PORTB &= ~(1 << LUMINANCE_PIN);
 
-			for (i = 0; i < 18; i++)// delay same amount to give proper back porch before drawing any pixels on the line
+			for (i = 0; i < HSYNC_BACKPORCH; i++)// delay same amount to give proper back porch before drawing any pixels on the line
 			{
 				__asm__ __volatile__ ("nop");
 			}
@@ -60,7 +67,7 @@ int main()
 			// Hold the output to the composite connector low, the zero volt hsync
 			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
 			DDRB |= (1 << COMPOSITE_PIN); // set COMPOSITE_PIN as output
-			for (i = 0; i < 18; i++)
+			for (i = 0; i < HSYNC_BACKPORCH; i++)
 			{
 				__asm__ __volatile__ ("nop");
 			}
@@ -72,13 +79,26 @@ int main()
 			{
 				__asm__ __volatile__ ("nop");
 			}
+			xPos = 0;
 		}
+
+		xPos++;
 
 		// the problem is we don't really know where in the line we are, we need a blanking period at end of line as well
 		if (pixels)
 		{
-			DDRB |= 1 << LUMINANCE_PIN;
-			PORTB |= 1 << LUMINANCE_PIN;
+			if (xPos == barStartPosition)
+			{
+				// turn white "pixel pin" on
+				DDRB |= 1 << LUMINANCE_PIN;
+				PORTB |= 1 << LUMINANCE_PIN;
+			}
+			if (xPos >= barStartPosition+50) // make this test bar 50 "pixels" wide)
+			{
+				// turn white "pixel pin" off
+				DDRB &= ~(1 << LUMINANCE_PIN);
+				PORTB &= ~(1 << LUMINANCE_PIN);
+			}
 		}
 		else
 		{
@@ -90,8 +110,8 @@ int main()
 		switch (lineCounter)
 		{
 			case 1: vSync = 0; pixels = 0; break;
-			case 20: pixels = 1; break; // only start pixel drawing after these many lines
-			case 50: pixels = 0; break; // pixels off
+			case 60: pixels = 1; break; // only start pixel drawing after these many lines
+			case 70: pixels = 0; break; // pixels off
 			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; pixels = 0; break;
 			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; pixels = 0;break;
 		}
