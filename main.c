0d508faf4d79c2bd566d05eef535e74891ddc0c5
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-10-29 08:50:13 +0100

    Update main.c
    extra code for the back porch

--- main.c
+++ main.c
@@ -12,12 +12,13 @@ int main()
 	uint16_t lineCounter = 0;
 	uint8_t vSync = 0;
 	uint8_t i = 0;
+	uint8_t	pixels = 1;
 
 	DDRB |= 1 << COMPOSITE_PIN;         //configure led as input
 	DDRB |= 1 << LUMINANCE_PIN;
 
 	TCCR1B = (1<<CS10);// | (1<<CS12); //set the pre-scalar as 1024
-	OCR1A = 805; 	   //usec delay
+	OCR1A = 1025; 	   //usec delay
 	TCNT1 = 0;
 	while(1)
 	{
@@ -28,34 +29,50 @@ int main()
 		}
 
 
-		if (vSync) // invert the line sync pulses when in vsync part of screen
+		if (vSync > 0) // invert the line sync pulses when in vsync part of screen
 		{
 			DDRB &= ~(1 << COMPOSITE_PIN);
 			for (i = 0; i < 18; i++)
 			{
 				__asm__ __volatile__ ("nop");
 			}
-			PORTB = 0;
+			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
 			DDRB |= (1 << COMPOSITE_PIN);
 		}
 		else
 		{
-			PORTB = 0; // set all PORTB to low, as DDRB in next statement is input this causes output to go low
+			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
 			DDRB |= (1 << COMPOSITE_PIN); // set COMPOSITE_PIN as output
 			for (i = 0; i < 18; i++)
 			{
 				__asm__ __volatile__ ("nop");
 			}
 			DDRB &= ~(1 << COMPOSITE_PIN); // set COMPOSITE_PIN as input
-			PORTB = 1; // set all PORTB to high, as DDRB in input this causes output to go high
+			PORTB |= 1 << COMPOSITE_PIN; // set PORTB COMPOSITE_PIN to high, as DDRB in input this causes output to go high
+
+			for (i = 0; i < 18; i++)// delay same amount to give proper back porch before drawing any pixels on the line
+			{
+				__asm__ __volatile__ ("nop");
+			}
 		}
+
+		// the problem is we don't really know where in the line we are, we need a blanking period at end of line as well
+		if (pixels)
+		{
+			DDRB |= 1 << LUMINANCE_PIN;
+			PORTB |= 1 << LUMINANCE_PIN; // set PORTB COMPOSITE_PIN to high
+		}
+
 		lineCounter++;
 		switch (lineCounter)
 		{
-			case 1: vSync = 0; break;
-			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; break;
-			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; break;
+			case 1: vSync = 0; pixels = 0; break;
+			case 20: pixels = 1; break; // only start pixel drawing after these many lines
+			case 50: pixels = 0; break; // pixels off
+			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; pixels = 0; break;
+			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; pixels = 0;break;
 		}
+
 
 		TCNT1 = 0;
 		TIFR1 |= (1<<OCF1A) ; //clear timer1 overflow flag
