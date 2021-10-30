4d6c4a2c69f4b039324118d9588acfa83560e10c
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-10-30 17:01:12 +0100

    updating screen from memory
    still issues with timing, such as the start of the loop is causing more of a delay so width of pixels is not uniform on byte boundaries.

--- main.c
+++ main.c
@@ -9,17 +9,21 @@
 #define COMPOSITE_PIN PB5
 #define LUMINANCE_PIN PB4
 #define MAX_LINE_BEFORE_BLANK 312
+#define FIRST_LINE_DRAWN 28
+#define LAST_LINE_DRAWN 200
+#define HALF_SCREEN 100
+
 
 #define HSYNC_BACKPORCH 18
-#define HSYNC_FRONT_PORCH_1 2
-#define HSYNC_FRONT_PORCH_2 16
+#define HSYNC_FRONT_PORCH_2 15
 
 // from experimenting a line started at linCounter = 25 appears right at top of screen one at 285 to 286 is bottom
 // a delay in clock cycles of 14 to plus a delay of 148 clock cycles gives a usable horizontal line length
 
-//XSIZE is in bytes
-#define XSIZE 18
-#define YSIZE 233
+// XSIZE is in bytes, and currently due to lack of time (spare clock cycles) only able todo 8 *8(bits) pixels = 64
+// will need some serious optimisation later!
+#define XSIZE 8
+#define YSIZE (HALF_SCREEN*2)
 
 
 
@@ -34,21 +38,25 @@ int main()
 	uint8_t screenLine = 0;
 	uint8_t pixelOn = 1;
 	uint8_t lineLength = 0;
+	uint8_t everyOther = 0;
+
+	// looking at the data sheet not sure if you have to do this in 3 instructions or not
+	CLKPR = 0b10000000; // set the CLKPCE bit to enable the clock prescaler to be changed
+	CLKPR = 0b10000000; // zero the CLKPS3 CLKPS2 CLKPS1 CLKPS0 bits to disable and clock division
+	CLKPR = 0b00000000; // clear CLKPCE bit
 
 	// setup a test pattern
-	uint8_t flag = 0;
-	uint8_t bits = 0b01010101;
-	uint8_t bitPat = 0;
-	for (uint8_t y = 0; y < YSIZE; y++)
-		for (uint8_t x = 0; x < XSIZE; x++)
+	for (uint8_t y = 0; y < YSIZE; y+=2)
+	{
+		for (uint8_t x = 0; x < XSIZE; x+=2)
 		{
-			screenMemory[y][x] = bitPat;
-			flag = 1 - flag;
-			if (flag == 1)
-			{
-				bitPat = bits;
-			}
+			// primative checkboard
+			screenMemory[y][x]  = 0xAA;
+			screenMemory[y][x+1]  = 0x55;
+			screenMemory[y+1][x]  = 0x55;
+			screenMemory[y+1][x+1]  = 0xAA;
 		}
+	}
 
 
 
@@ -65,6 +73,7 @@ int main()
 		{
 			// wait till the timer overflow flag is SET
 		}
+
 
 		// immediately reset the interrupt timer, previous version had this at the end
 		// which made the whole thing be delayed when we're trying to maintain the 64us PAL line timing
@@ -90,11 +99,6 @@ int main()
 			DDRB &= ~(1 << LUMINANCE_PIN);
 			PORTB &= ~(1 << LUMINANCE_PIN);
 
-			for (i = 0; i < HSYNC_FRONT_PORCH_1; i++)// delay same amount to give proper back porch before drawing any pixels on the line
-			{
-				__asm__ __volatile__ ("nop");
-			}
-
 			// Hold the output to the composite connector low, the zero volt hsync
 			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
 			DDRB |= (1 << COMPOSITE_PIN); // set COMPOSITE_PIN as output
@@ -116,40 +120,76 @@ int main()
 		// the whole line writing must be a total of less than (64 - (18 * 3)) = 10usec =
 		// a delay in line 1 = 1/16000000 =0.0000000625 assuming nop = 1 clock cycle
 
-		if (drawPixelsOnLine)
+
+		// flag "everyOther" ensures we update half the pixels on alternate frames
+		// this is similar to the PAL interlacing, and gives us more clock cycles for
+		// game code!
+		if ((drawPixelsOnLine) && (everyOther))
 		{
-			for (i = 0; i < 25; i++)
+			for (uint8_t x = 0; x < XSIZE; x++)
 			{
-				__asm__ __volatile__ ("nop");
+				uint8_t bits = screenMemory[lineCounter][x];
+
+				if (bits & (1 <<  0))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  1))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  2))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  3))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  4))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  5))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  6))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
+				else
+					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
+				if (bits & (1 <<  7))
+					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
 			}
-			DDRB |= (1 << LUMINANCE_PIN); // pixel on
-			PORTB |= (1 << LUMINANCE_PIN); // pixel on
-			for (i = 0; i < lineLength; i++)
-			{
-				__asm__ __volatile__ ("nop");
-			}
-			DDRB &= ~(1 << LUMINANCE_PIN); // pixel off
-			PORTB &= ~(1 << LUMINANCE_PIN); // pixel off
-			lineLength+=3;
 		}
-
-
 		lineCounter++;
 		switch (lineCounter)
 		{
 			case 1: vSync = 0; break;
-			case 40: screenLine = 0; drawPixelsOnLine = 1; lineLength = 0; break;
-			case 80: drawPixelsOnLine = 0;  lineLength = 0; break;
-			case 81: screenLine = 0; drawPixelsOnLine = 1; lineLength = 0; break;
-			case 120: drawPixelsOnLine = 0;  lineLength = 0; break;
-			case 121: screenLine = 0; drawPixelsOnLine = 1; lineLength = 0; break;
-			case 160: drawPixelsOnLine = 0;  lineLength = 0; break;
-			case 161: screenLine = 0; drawPixelsOnLine = 1; lineLength = 0; break;
-			case 200: drawPixelsOnLine = 0;  lineLength = 0; break;
-			case 201: screenLine = 0; drawPixelsOnLine = 1; lineLength = 0; break;
-			case 240: drawPixelsOnLine = 0;  lineLength = 0; break;
+			case FIRST_LINE_DRAWN: screenLine = 0; drawPixelsOnLine = 1; break;
+			case LAST_LINE_DRAWN: drawPixelsOnLine = 0;
+								DDRB &= ~(1 << LUMINANCE_PIN); // pixel off
+								PORTB &= ~(1 << LUMINANCE_PIN); // pixel off
+			break;
 			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; drawPixelsOnLine = 0; break;
-			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; break;
+			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; everyOther = 1 - everyOther; break;
 		}
 	}
 }
+
+
+#if 0
+else
+{
+	DDRB |= (1 << LUMINANCE_PIN); // pixel on
+	PORTB |= (1 << LUMINANCE_PIN); // pixel on
+	for (i = 0; i < 205; i++)
+	{
+		__asm__ __volatile__ ("nop");
+	}
+
+	DDRB &= ~(1 << LUMINANCE_PIN); // pixel off
+	PORTB &= ~(1 << LUMINANCE_PIN); // pixel off
+}
+
+#endif
