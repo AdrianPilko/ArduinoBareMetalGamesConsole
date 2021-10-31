d008bb19af67394b9221fe6d29500a3f4f0e09de
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-10-31 20:33:35 +0000

    fixed timing issues, changed test parttern
    - now draws screen memory reliably no matter if is black or white, the timing for either is identical.
    - changed test pattern to initials and all lines up correctly
    - now ready for next stage which is to implement actual game code

--- main.c
+++ main.c
@@ -9,9 +9,9 @@
 #define COMPOSITE_PIN PB5
 #define LUMINANCE_PIN PB4
 #define MAX_LINE_BEFORE_BLANK 312
-#define FIRST_LINE_DRAWN 28
-#define LAST_LINE_DRAWN 250
-#define HALF_SCREEN 125
+#define FIRST_LINE_DRAWN 42
+#define LAST_LINE_DRAWN 262
+#define HALF_SCREEN 110
 
 
 #define HSYNC_BACKPORCH 18
@@ -33,12 +33,7 @@ int main()
 	uint8_t  vSync = 0;
 	uint8_t i = 0;
 	uint8_t drawPixelsOnLine = 0;
-	uint8_t afterStartOfLineSync = 0;
 	uint8_t screenMemory[YSIZE][XSIZE];
-	uint8_t screenLine = 0;
-	uint8_t pixelOn = 1;
-	uint8_t lineLength = 0;
-	uint8_t everyOther = 0;
 	uint8_t yCounter = 0;
 
 	// looking at the data sheet not sure if you have to do this in 3 instructions or not
@@ -47,35 +42,59 @@ int main()
 	CLKPR = 0b00000000; // clear CLKPCE bit
 
 	memset (&screenMemory[0][0],0,sizeof(uint8_t) * YSIZE * XSIZE );
-	// setup a 10 line checkerboard in memory for testing
+
+	// setup a test pattern in screen memory for testing
+	// consists of my initials repeated
 	for (uint8_t y = 0; y < YSIZE; y+=20)
 	{
-		for (uint8_t x = 0; x < XSIZE; x++)
+		for (uint8_t x = 0; x < XSIZE; x+=2)
 		{
-			screenMemory[y][x]  =  0b00010000;
-			screenMemory[y+1][x] = 0b00010000;
-			screenMemory[y+2][x] = 0b00010000;
-			screenMemory[y+3][x] = 0b00010000;
-			screenMemory[y+4][x] = 0b11111111;
-			screenMemory[y+5][x] = 0b11111111;
-			screenMemory[y+6][x] = 0b00010000;
-			screenMemory[y+7][x] = 0b00010000;
-			screenMemory[y+8][x] = 0b00010000;
-			screenMemory[y+9][x] = 0b00010000;
+			screenMemory[y][x]  =  0b00000000;
+			screenMemory[y+1][x] = 0b00011000;
+			screenMemory[y+2][x] = 0b00100100;
+			screenMemory[y+3][x] = 0b01000010;
+			screenMemory[y+4][x] = 0b01000010;
+			screenMemory[y+5][x] = 0b01111110;
+			screenMemory[y+6][x] = 0b01000010;
+			screenMemory[y+7][x] = 0b01000010;
+			screenMemory[y+8][x] = 0b01000010;
+			screenMemory[y+9][x] = 0b00000000;
 
-			screenMemory[y+10][x]  = 0b00010000;
-			screenMemory[y+11][x]  = 0b00010000;
-			screenMemory[y+12][x]  = 0b00010000;
-			screenMemory[y+13][x]  = 0b00010000;
-			screenMemory[y+14][x]  = 0b00010000;
-			screenMemory[y+15][x]  = 0b00010000;
-			screenMemory[y+16][x]  = 0b00010000;
-			screenMemory[y+17][x]  = 0b00010000;
-			screenMemory[y+18][x]  = 0b00010000;
-			screenMemory[y+19][x]  = 0b00010000;
+			screenMemory[y][x+1]  =  0b00000000;
+			screenMemory[y+1][x+1] = 0b00111100;
+			screenMemory[y+2][x+1] = 0b01000100;
+			screenMemory[y+3][x+1] = 0b01000100;
+			screenMemory[y+4][x+1] = 0b01000100;
+			screenMemory[y+5][x+1] = 0b00111100;
+			screenMemory[y+6][x+1] = 0b00000100;
+			screenMemory[y+7][x+1] = 0b00000100;
+			screenMemory[y+8][x+1] = 0b00000100;
+			screenMemory[y+9][x+1] = 0b00000000;
+
+
+			screenMemory[y+10][x]  = 0b00000000;
+			screenMemory[y+11][x]  = 0b00000000;
+			screenMemory[y+12][x]  = 0b00000000;
+			screenMemory[y+13][x]  = 0b11111111;
+			screenMemory[y+14][x]  = 0b00000000;
+			screenMemory[y+15][x]  = 0b00000000;
+			screenMemory[y+16][x]  = 0b11111111;
+			screenMemory[y+17][x]  = 0b00000000;
+			screenMemory[y+18][x]  = 0b00000000;
+			screenMemory[y+19][x]  = 0b00000000;
+
+			screenMemory[y+10][x+1]  = 0b00000000;
+			screenMemory[y+11][x+1]  = 0b00000000;
+			screenMemory[y+12][x+1]  = 0b00000000;
+			screenMemory[y+13][x+1]  = 0b00000000;
+			screenMemory[y+14][x+1]  = 0b11111111;
+			screenMemory[y+15][x+1]  = 0b11111111;
+			screenMemory[y+16][x+1]  = 0b00000000;
+			screenMemory[y+17][x+1]  = 0b00000000;
+			screenMemory[y+18][x+1]  = 0b00000000;
+			screenMemory[y+19][x+1]  = 0b00000000;
 		}
 	}
-
 
 
 	DDRB |= 1 << COMPOSITE_PIN;
@@ -100,23 +119,23 @@ int main()
 
 		if (vSync > 0) // invert the line sync pulses when in vsync part of screen
 		{
-			DDRB &= ~(1 << COMPOSITE_PIN);
-			for (i = 0; i < HSYNC_BACKPORCH; i++)
+			DDRB = 0;
+			for (i = 0; i < HSYNC_BACKPORCH+10; i++)
 			{
 				__asm__ __volatile__ ("nop");
 			}
-			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
-			DDRB |= (1 << COMPOSITE_PIN);
+			PORTB = 0;
+			DDRB = 1;
 		}
 		else // hsync
 		{
 			// before the end of line (which in here is also effectively just the start of the line!) we need to hold at 300mV
 			// and ensure no pixels
-			DDRB &= ~(1 << COMPOSITE_PIN); // set COMPOSITE_PIN as input
-			PORTB |= 1 << COMPOSITE_PIN; // set PORTB COMPOSITE_PIN to high, as DDRB in input this causes output to go high
+			DDRB = 0;
+			PORTB = 1;
 
 			// Hold the output to the composite connector low, the zero volt hsync
-			PORTB &= 0 << COMPOSITE_PIN; // set all PORTB COMPOSITE_PIN to low, as DDRB in next statement is input this causes output to go low
+			PORTB = 0;
 			DDRB |= (1 << COMPOSITE_PIN); // set COMPOSITE_PIN as output
 			for (i = 0; i < HSYNC_BACKPORCH; i++)
 			{
@@ -125,76 +144,58 @@ int main()
 
 			// hold output to composite connector to 300mV
 			DDRB &= ~(1 << COMPOSITE_PIN); // set COMPOSITE_PIN as input
-			PORTB |= 1 << COMPOSITE_PIN; // set PORTB COMPOSITE_PIN to high, as DDRB in input this causes output to go high
-			for (i = 0; i < HSYNC_BACKPORCH+10; i++)// delay same amount to give proper back porch before drawing any pixels on the line
+			PORTB = 1;
+			for (i = 0; i < HSYNC_BACKPORCH; i++)// delay same amount to give proper back porch before drawing any pixels on the line
 			{
 				__asm__ __volatile__ ("nop");
 			}
-			afterStartOfLineSync = 1;
 		}
 
 		// the whole line writing must be a total of less than (64 - (18 * 3)) = 10usec =
 		// a delay in line 1 = 1/16000000 =0.0000000625 assuming nop = 1 clock cycle
 
+		#define  LUM_ON { DDRB = PORTB = 0b00011000; }
+		// have to add this nop to get timing right in clock
+		// cycles i.e. LUM_ON and LUM_OFF must take exactly same time
+		#define  LUM_OFF { DDRB = PORTB = 0; __asm__ __volatile__ ("nop");}
 
-		// flag "everyOther" ensures we update half the pixels on alternate frames
-		// this is similar to the PAL interlacing, and gives us more clock cycles for
-		// game code!
-		if (drawPixelsOnLine)//&& (everyOther))
+		if (drawPixelsOnLine)
 		{
+			for (i = 0; i < 15; i++)
+			{
+				__asm__ __volatile__ ("nop");
+			}
 			uint8_t * loopPtrMax = &screenMemory[yCounter][XSIZE-1];
 			uint8_t * OneLine = &screenMemory[yCounter][0];
 			do {
-				if (*OneLine & (1 <<  0))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  1))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  2))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  3))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  4))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  5))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  6))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-				else
-					{DDRB &= ~(1 << LUMINANCE_PIN); PORTB &= ~(1 << LUMINANCE_PIN); }
-				if (*OneLine & (1 <<  7))
-					{DDRB |= (1 << LUMINANCE_PIN); PORTB |= (1 << LUMINANCE_PIN);}
-
+				if (*OneLine & (1 <<  0)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  1)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  2)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  3)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  4)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  5)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  6)) LUM_ON else LUM_OFF
+				if (*OneLine & (1 <<  7)) LUM_ON else LUM_OFF
 				OneLine++;
 			}
 			while (OneLine <= loopPtrMax);
 		}
-		DDRB &= ~(1 << LUMINANCE_PIN); // pixel off
-		PORTB &= ~(1 << LUMINANCE_PIN); // pixel off
+
+		LUM_OFF
+
 		lineCounter++;
 		if (drawPixelsOnLine) yCounter++;
 
 		switch (lineCounter)
 		{
 			case 1: vSync = 0; break;
-			case FIRST_LINE_DRAWN: screenLine = 0; drawPixelsOnLine = 1; break;
+			case FIRST_LINE_DRAWN: drawPixelsOnLine = 1; break;
 			case LAST_LINE_DRAWN: drawPixelsOnLine = 0; yCounter = 0;
 								DDRB &= ~(1 << LUMINANCE_PIN); // pixel off
 								PORTB &= ~(1 << LUMINANCE_PIN); // pixel off
 			break;
 			case MAX_LINE_BEFORE_BLANK-6: vSync = 1; drawPixelsOnLine = 0; break;
-			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; everyOther = 1 - everyOther; break;
+			case MAX_LINE_BEFORE_BLANK: lineCounter = 0; vSync = 0; break;
 		}
 	}
 }
